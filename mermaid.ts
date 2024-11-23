bd3488d1fd99113a249288ad527c466da6d824ac
Michael Kolb <dev@goroot.de>
2024-11-23 21:39:11 +0100

    changed cdn provider

--- mermaid.ts
+++ mermaid.ts
@@ -4,7 +4,7 @@ export function widget(
   return {
     html: `<pre class="mermaid">${bodyText.replaceAll("<", "&lt;")}</pre>`,
     script: `
-    loadJsByUrl("https://cdn.jsdelivr.net/npm/mermaid@10.9.3/dist/mermaid-5a5980d4.js","sha256-oXlHg6q3LSBdxTKxFw0b5j69zogWtXwhrLRRwV2rlpo=").then(() => {
+    loadJsByUrl("https://cdnjs.cloudflare.com/ajax/libs/mermaid/10.9.3/mermaid.min.js","sha512-HvxxeyPSnbU7/x0g15v3OMxTFeADyCUnCN3iCam3BDTxgFPKxa+ujRCbFuwjE8PASDwOH5LpzFfGGNWks7tuJQ==").then(() => {
       mermaid.init().then(updateHeight);
     });
     document.addEventListener("click", () => {
